33bd41c4c4b438e90db4dca53d88ebb119c89650
JoshDaos <joshuadaos1998@gmail.com>
2021-10-09 10:43:51 -0700

    working on hooks nad data sending

--- fb-react-app/src/App.js
+++ fb-react-app/src/App.js
@@ -1,18 +1,33 @@
 import { useState, useEffect } from 'react';
 import './App.css';
+import { db } from './firebase-config';
+import { collection, getDocs } from 'firebase/firestore';
+
+
+
 
 function App() {
 
   const [user, setUsers] = useState([]);
-
+  const usersCollectionRef = collection(db, "user")
   useEffect(() => {
 
+
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef)
+        setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id })));
+
+      // console.log(data);
+    }
+
+    getUsers()
   }, [])
 
-  
+
   return (
     <div className="App">
       <h1>firebase project</h1>
+      
     </div>
   );
 }
